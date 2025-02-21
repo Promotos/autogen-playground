25837231e6c90ccd8e942c10000d74482a50acec
Promotos <promotos@gmx.de>
2025-02-21 13:42:16 +0100

    update

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # AutoGen Playground
 
 [AutoGen Project Page]([https://](https://microsoft.github.io/autogen/stable/index.html))
+
+## Install
+```
+pip install "autogen-core"
+pip install "autogen-ext[openai]"
+```
---
+++ simple_local_ollama/simple_local_ollama.py
@@ -0,0 +1,23 @@
+import asyncio
+from autogen_core.models import UserMessage
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+model_client = OpenAIChatCompletionClient(
+    model="deepseek-r1",
+    base_url="http://localhost:11434/v1",
+    api_key="placeholder",
+    model_info={
+        "vision": False,
+        "function_calling": True,
+        "json_output": False,
+        "family": "unknown",
+    },
+)
+
+
+async def main():
+    response = await model_client.create([UserMessage(content="What is the capital of France?", source="user")])
+    print(response)
+
+
+asyncio.run(main())
